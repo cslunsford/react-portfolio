1eaa8fe0f29ca00e0be5dedd4839c44598c26545
cslunsford <christopher.lunsford@uconn.edu>
2023-11-09 15:50:13 -0500

    fixed image paths

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,7 +5,7 @@ function Homepage() {
         <div>
             <h2>About Me</h2>
             <div>
-                <img src='../assets/ProfilePicture.png' alt='Profile picture'></img>
+                <img src='/ProfilePicture.png' alt='Profile picture'></img>
             </div>
             <div>
                 <p>Hello! My name is Christopher Lunsford and I am a 26 year old web-developer based out of Connecticut. I participated in a 24-week-long coding bootcamp where I learned both front-end and back-end development. I've become proficient in multiple technologies such as JavaScript, Node, SQL, MongoDB, React, and more! Please explore the site to learn more about projects I've taken part in, and be sure to checkout my GitHub and LinkedIn linked in the footer of the page!</p>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,32 +4,32 @@ import Project from '../components/Project';
 const projects = [
     {
         title: 'Wallet Whiz',
-        image: '../assets/WalletWiz.png',
+        image: '/WalletWiz.png',
         deployedLink: 'https://walletwizzz-e48c873597e2.herokuapp.com/homepage',
         repoLink: 'https://github.com/cslunsford/wallet-wiz',
     }, {
         title: 'Cooking With What You Got',
-        image: '../assets/CookingWithWhatYouGot',
+        image: '/CookingWithWhatYouGot',
         deployedLink: 'https://lukiangelxd.github.io/Cookingwithwhatyougot/',
         repoLink: 'https://github.com/Lukiangelxd/Cookingwithwhatyougot',
     }, {
         title: 'Book Search Engine Refactor',
-        image: '../assets/BookSearchEngine.png',
+        image: '/BookSearchEngine.png',
         deployedLink: 'https://calm-inlet-40932-ce4898dd1f2a.herokuapp.com/',
         repoLink: 'https://github.com/cslunsford/book-search-engine',
     }, {
         title: 'Note Taker App',
-        image: '../assets/NoteTakerApp.png',
+        image: '/NoteTakerApp.png',
         deployedLink: 'https://github.com/cslunsford/note-taker-app#readme',
         repoLink: 'https://github.com/cslunsford/note-taker-app',
     }, {
         title: 'Social Network API',
-        image: '../assets/SocialNetworkAPI.png',
+        image: '/SocialNetworkAPI.png',
         deployedLink: 'https://drive.google.com/file/d/1OCUMadj8j1r1LbJMOk_LmELRKp4a8HMi/view',
         repoLink: 'https://github.com/cslunsford/social-network-api',
     }, {
         title: 'Ecommerce Backend',
-        image: '../assets/EcommerceBackend.png',
+        image: '/EcommerceBackend.png',
         deployedLink: 'https://drive.google.com/file/d/1IhcQt76UhR1hanOq0ca75cr4AXm1G5ej/view?pli=1',
         repoLink: 'https://github.com/cslunsford/ecommerce-backend',
     }
