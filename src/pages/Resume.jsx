47414efb7f239e3be172afc1285d7692c94613dd
cslunsford <christopher.lunsford@uconn.edu>
2023-11-15 16:24:21 -0500

    changed icon styling

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -10,19 +10,19 @@ function Resume() {
                     <ListGroup className='customListGroup'>
                     <h3>Front-End</h3>
                         <ListGroup.Item className='customListItem'>
-                            <i className='fa-solid fa-code'></i>HTML
+                            <i className='fa-solid fa-code me-2'></i>HTML
                         </ListGroup.Item>
                         <ListGroup.Item className='customListItem'>
-                            <i className='fab fa-css3'></i>CSS
+                            <i className='fab fa-css3 me-2'></i>CSS
                         </ListGroup.Item>
                         <ListGroup.Item className='customListItem'>
-                            <i className='fab fa-bootstrap'></i>Bootstrap
+                            <i className='fab fa-bootstrap me-2'></i>Bootstrap
                         </ListGroup.Item>
                         <ListGroup.Item className='customListItem'>
-                            <i className='fab fa-js'></i>JavaScript & jQuery
+                            <i className='fab fa-js me-2'></i>JavaScript & jQuery
                         </ListGroup.Item>
                         <ListGroup.Item className='customListItem'>
-                            <i className='fab fa-react'></i>React
+                            <i className='fab fa-react me-2'></i>React
                         </ListGroup.Item>
                     </ListGroup>
                 </Col>
@@ -30,22 +30,22 @@ function Resume() {
                     <ListGroup className='customListGroup'>
                     <h3>Back-End</h3>
                         <ListGroup.Item className='customListItem'>
-                            <i className='fab fa-node'></i>Node.js
+                            <i className='fab fa-node me-2'></i>Node.js
                         </ListGroup.Item>
                         <ListGroup.Item className='customListItem'>
-                            <i className='fa-solid fa-server'></i>Express
+                            <i className='fa-solid fa-server me-2'></i>Express
                         </ListGroup.Item>
                         <ListGroup.Item className='customListItem'>
-                            <i className='fa-solid fa-table'></i>SQL
+                            <i className='fa-solid fa-table me-2'></i>SQL
                         </ListGroup.Item>
                         <ListGroup.Item className='customListItem'>
-                            <i className='fab fa-js'></i>JavaScript
+                            <i className='fab fa-js me-2'></i>JavaScript
                         </ListGroup.Item>
                         <ListGroup.Item className='customListItem'>
-                            <i className='fa-solid fa-database'></i>MongoDB
+                            <i className='fa-solid fa-database me-2'></i>MongoDB
                         </ListGroup.Item>
                         <ListGroup.Item className='customListItem'>
-                            <i className='fa-solid fa-network-wired'></i>GraphQL
+                            <i className='fa-solid fa-network-wired me-2'></i>GraphQL
                         </ListGroup.Item>
                     </ListGroup>
                 </Col>
