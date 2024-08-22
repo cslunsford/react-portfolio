eeb21e7c24ccd393d6c4a33eda43263f3072989f
cslunsford <christopher.lunsford@uconn.edu>
2024-08-22 01:07:12 -0400

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import '../App.css';
 function Footer() {
     return (
         <footer>
-            <h2>Check out my GitHub and LinkedIn here!</h2>
+            <h2>Check out my GitHub here!</h2>
             <ul className='list-unstyled d-flex justify-content-center'>
                 <li className='me-5'>
                     <a href='https://github.com/cslunsford' target='_blank' rel='noopener noreferrer'>
